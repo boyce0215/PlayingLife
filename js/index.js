709897ffa1add920bdb60b8157a0cf14dfea62d6
SOSO <SOSO@soso-MacBook-Pro.local>
2017-12-20 23:04:30 +0800

    1220 1.新增忘記密碼、寄送確認信的彈窗 2.變更nav的底色 3.新增會員帳號

--- js/index.js
+++ js/index.js
@@ -1,10 +1,18 @@
-$("#login a").click(function(){
+$("#login #goto-register").click(function(){
   $("#login").modal("hide");
   $("#register").modal("show");
 });
-$("#register a").click(function(){
+$("#login #goto-forget").click(function(){
+  $("#login").modal("hide");
+  $("#forget").modal("show");
+});
+$("#register #goto-login").click(function(){
   $("#register").modal("hide");
   $("#login").modal("show");
+});
+$("#forget #goto-sent").click(function(){
+	$("#forget").modal("hide");
+	$("#sent").modal("show");
 });
 $("#logout-button").click(function(){
   window.location.href = '/accounts/logout/';
