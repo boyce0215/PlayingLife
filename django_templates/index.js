da044b0122b36b07355d808f4e17d2e103ea32fc
SOSO <SOSO@soso-MacBook-Pro.local>
2017-12-28 17:59:28 +0800

    1228 1.新增倒數器 2.彈窗樣式微調（顏色，文字位置） 3.文字增加陰影 4.新增登入會後顯示已獲得幾杯星巴克 5.雲端字體真的成功了！！

--- django_templates/index.js
+++ django_templates/index.js
@@ -1,3 +1,102 @@
+/*倒數器開始*/
+var cset, countdown1 = {
+  settings: {
+    dateStart: new Date('Dec 28, 2017 00:00:00'),/*設定開始日期*/
+    dateEnd: new Date('Dec 29, 2017 00:00:00'),/*設定結束日期*/
+    elements: document.querySelectorAll(".timer"),/*設定哪一個選擇器*/
+    msgBefore: "活動已結束!",/*開始日期前會出現的內容*/
+    msgAfter: "活動未開始!",/*結束日期後會出現的內容*/
+    msgPattern: '<span> {hours} 小時 {minutes} 分  {seconds} 秒 後預測截止</span>',/*活動期間會出現的內容*/
+    patterns: [{
+      pattern: '{years}',
+      secs: 31536000,
+    }, {
+      pattern: '{months}',
+      secs: 2628000,
+    }, {
+      pattern: '{weeks}',
+      secs: 604800,
+    }, {
+      pattern: '{days}',
+      secs: 86400
+    }, {
+      pattern: '{hours}',
+      secs: 3600
+    }, {
+      pattern: '{minutes}',
+      secs: 60
+    }, {
+      pattern: '{seconds}',
+      secs: 1
+    }],
+    interval: 1000,
+    now: new Date()
+  },
+
+  init: function() {
+    cset = this.settings;
+    this.defineInterval();
+    cset.now < cset.dateEnd && cset.now > cset.dateStart ? this.run() : this.outOfInterval();
+  },
+
+  run: function() {
+    var nowTS = cset.now.valueOf() / 1000,
+      tarTS = cset.dateEnd.valueOf() / 1000,
+      sec = Math.abs(tarTS - nowTS);
+
+    var timer = setInterval(function() {
+      sec--;
+
+      if (sec > 0) {
+        countdown1.display(sec);
+
+      } else {
+        countdown1.outOfInterval();
+        clearInterval(timer);
+      }
+    }, cset.interval);
+
+    countdown1.display(sec);
+  },
+
+  defineInterval: function() {
+    for (var i = cset.patterns.length; i > 0; i--) {
+      var currentPattern = cset.patterns[i - 1];
+
+      if (cset.msgPattern.indexOf(currentPattern.pattern) !== -1) {
+        cset.interval = currentPattern.secs * 1000;
+        return;
+      }
+    }
+  },
+
+  display: function(sec) {
+    var output = cset.msgPattern;
+
+    for (var i = 0, len = cset.patterns.length; i < len; i++) {
+      var currentPattern = cset.patterns[i];
+
+      if (cset.msgPattern.indexOf(currentPattern.pattern) !== -1) {
+        var number = Math.floor(sec / currentPattern.secs);
+        sec -= number * currentPattern.secs;
+        output = output.replace(currentPattern.pattern, number);
+      }
+    }
+
+    for (var i = 0, len = cset.elements.length; i < len; i++)
+      cset.elements[i].innerHTML = output;
+  },
+
+  outOfInterval: function() {
+    var message = cset.now > cset.dateStart ? cset.msgBefore : cset.msgAfter;
+    for (var i = 0, len = cset.elements.length; i < len; i++)
+      cset.elements[i].innerHTML = message;
+  }
+}
+
+countdown1.init();
+/*倒數器結束*/
+
 $("#goto-play").click(function(){
   $("html,body").animate({
     scrollTop: 680
